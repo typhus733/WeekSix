fc3427fbe3ece59b01904d7ca6eb56a09f523e70
Brandon Gunthner <typhus733@gmail.com>
2021-10-18 21:22:40 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WeekSix
+{
+    public class BadInt : Exception
+    {
+        public BadInt()
+        {
+        }
+
+        public BadInt(string message) : base(message)
+        {
+        }
+
+        public BadInt(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+    class Program
+    {
+
+
+        static void NumberSeven()
+        {
+            try
+            {
+                Console.WriteLine("Enter a positive integer");
+                int input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    throw new BadInt();
+                }
+
+                Console.WriteLine(Math.Sqrt(input));
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+
+            catch (BadInt bi)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            NumberSeven();
+        }
+    }
+}
