1062a5caf18eed69ccde4a370c2733d7a02a4807
Brandon Gunthner <typhus733@gmail.com>
2021-10-19 20:51:02 -0500

    Finished Number Eight

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WeekSix
 {
@@ -10,6 +11,7 @@ namespace WeekSix
 
         public BadInt(string message) : base(message)
         {
+            Console.WriteLine(message);
         }
 
         public BadInt(string message, Exception inner) : base(message, inner)
@@ -28,7 +30,7 @@ namespace WeekSix
                 int input = int.Parse(Console.ReadLine());
                 if (input < 0)
                 {
-                    throw new BadInt();
+                    throw new BadInt("Number must be positive");
                 }
 
                 Console.WriteLine(Math.Sqrt(input));
@@ -40,7 +42,7 @@ namespace WeekSix
 
             catch (BadInt bi)
             {
-                Console.WriteLine("Invalid Number");
+                
             }
 
             finally
@@ -50,10 +52,65 @@ namespace WeekSix
 
         }
 
+        static void ReadNumber(int start, int end)
+        {
+            int currentNum = start;
+            for(int x = 1; x <= 10; x++)
+            {
+                try
+                {
+                    Console.WriteLine("Enter number {2}, must be greater than {0} and less than {1}", currentNum, end, x);
+                    int input = int.Parse(Console.ReadLine());
+                    
+                    if (input < 0)
+                    {
+                        throw new BadInt("Bad number, cannot be negative");
+                    }
+                    else if (input <= currentNum)
+                    {
+                        throw new BadInt("Bad number, number too low");
+                    }
+                    else if (input >= (end - (10 - x)))
+                    {
+                        throw new BadInt("Bad number, number too high and/or potentially prevents full iteration");
+                    }
+                    currentNum = input;
+
+                }
+                catch(FormatException fe)
+                {
+                    x--;
+                    Console.WriteLine("Bad input! Must be an integer");
+                }
+
+                catch(BadInt bi)
+                {
+                    x--;
+                }
+            }
+
+        }
+
+        static string NumberNine(string fileName)
+        {
+            string fileRead = "";
+            try 
+            {
+                fileRead = File.ReadAllText(fileName);
+            }
+
+            catch (IOException e)
+            {
+                fileRead = "Bad file!";
+            }
+            return fileRead;
+        }
 
         static void Main(string[] args)
         {
-            NumberSeven();
+            //NumberSeven();
+            //ReadNumber(1, 100);
+            Console.WriteLine(NumberNine(@"C:\temp\junk.txt"));
         }
     }
 }
